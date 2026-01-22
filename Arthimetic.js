d88552853decfa5eacfae3cd3893377623350992
Vishalkumar8757 <vishalkumaroppo8757@gmail.com>
2026-01-23 00:53:39 +0530

    onlineclasses

---
+++ Arthimetic.js
@@ -0,0 +1,24 @@
+// arthimetic opetator
+
+let a = 2;
+let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a**b);
+
+
+// unary opetator
+// console.log(a++);
+// console.log(++a);
+// console.log(++b);
+// console.log(++b);
+
+//assignment operator
+console.log(a=b);
+console.log(a+=1);
+console.log(b-=1);
+console.log(a*=1);
+console.log(a/=1);
---
+++ index.js
@@ -0,0 +1,11 @@
+let a=5;
+let b = 10;
+
+console.log("Sum is :", a+b);
+
+let bookprice = 45;
+let notebookprice = 45;
+// let output = "The total prize is :" +( bookprice+notebookprice ) + " Rupees"
+let output = `The total prize is : ${bookprice+notebookprice} Rupees`
+
+console.log(output)
